00d43ee2918c9270f23a5e56babc7a9f0833c4c8
HWY <1833121448@qq.com>
2020-07-08 18:02:08 +0800

    修改样式问题

--- src/components/songListDetails.vue
+++ src/components/songListDetails.vue
@@ -3,93 +3,95 @@
     <div class="dialog-mask">
       <div class="dialog-wrapper animated fadeInUp" ref="dialogWrapper">
         <div class="dialog-container" ref="viewBox">
-          <!-- song banner -->
-          <div class="topBanner" :style="{background:backgroudURl}">
-            <div class="topnarBar">
-              <div id="back" @click="songListClose()">
-                <i class="iconfont iconfanhui"></i>
-                <span>{{songInformation.name}}</span>
+          <div class="containerDiaLog" v-if="rendering">
+            <!-- song banner -->
+            <div class="topBanner songListTopBanner" >
+              <div class="topnarBar">
+                <div id="back" @click="songListClose()">
+                  <i class="iconfont iconfanhui"></i>
+                  <span>{{songInformation.name}}</span>
+                </div>
+                <div class="more">
+                  <i class="iconfont iconwenhao1"></i>
+                </div>
               </div>
-              <div class="more">
-                <i class="iconfont iconwenhao1"></i>
-              </div>
-            </div>
 
-            <div class="information">
-              <div class="left">
-                <img :src="songInformation.coverImgUrl" alt="封面" />
-                <span>
-                  <i class="iconfont iconicon--"></i>
-                  {{songInformation.playCount|retainDoubleDigit}}万
-                </span>
-              </div>
-              <div class="rigth">
-                <div class="topTitle">
-                  <p>{{songInformation.name}}</p>
+              <div class="information">
+                <div class="left">
+                  <img :src="songInformation.coverImgUrl" alt="封面" />
+                  <span>
+                    <i class="iconfont iconicon--"></i>
+                    {{songInformation.playCount|retainDoubleDigit}}万
+                  </span>
                 </div>
-                <div class="user">
-                  <img :src="songInformation.creator.avatarUrl" alt="头像" />
-                  <span>{{songInformation.creator.nickname}}</span>
-                </div>
-                <div class="details">
-                  <p>{{songInformation.description}}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- song 列表 -->
-          <div class="bottomContainer">
-            <div class="bgc">
-              <!-- song 顶部装饰 -->
-              <div class="topStyle">
-                <div class="le">
-                  <div class>
-                    <span></span>
-                    <span></span>
+                <div class="rigth">
+                  <div class="topTitle">
+                    <p>{{songInformation.name}}</p>
                   </div>
-                </div>
-                <div class="ri">
-                  <div class>
-                    <span></span>
-                    <span></span>
+                  <div class="user">
+                    <img :src="songInformation.creator.avatarUrl" alt="头像" />
+                    <span>{{songInformation.creator.nickname}}</span>
+                  </div>
+                  <div class="details">
+                    <p>{{songInformation.description}}</p>
                   </div>
                 </div>
               </div>
-              <!-- song 工具栏 -->
-              <div class="songsBar">
-                <div class="left" @click="playMusicAll()">
-                  <i class="iconfont iconbofang-"></i>
-                  <span>播放全部 (共{{songInformation.trackCount}}首)</span>
-                </div>
-                <div class="right">
-                  <i class="iconfont icongengduo1"></i>
-                  <span>多选</span>
-                </div>
-              </div>
+            </div>
 
-              <!-- song 详情 -->
-              <div class="songWarp">
-                <ul>
-                  <li v-for="item in songplayList" :key="item.id" @click="playMusic(item)">
-                    <div class="left">
-                      <img :src="item.al.picUrl" :alt="item.al.name" />
+            <!-- song 列表 -->
+            <div class="bottomContainer">
+              <div class="bgc">
+                <!-- song 顶部装饰 -->
+                <div class="topStyle">
+                  <div class="le">
+                    <div class>
+                      <span></span>
+                      <span></span>
                     </div>
-                    <div class="center">
-                      <p>
-                        <span>{{item.name}}</span>&nbsp;
-                        <span class="popularity">{{item.alia[0]}}</span>
-                      </p>
-                      <p>{{item.ar[0].name}}</p>
+                  </div>
+                  <div class="ri">
+                    <div class>
+                      <span></span>
+                      <span></span>
                     </div>
-                    <div class="right">
-                      <div class="icon">
-                        <i class="iconfont iconbofang"></i>
-                        <i class="iconfont iconziyuan"></i>
+                  </div>
+                </div>
+                <!-- song 工具栏 -->
+                <div class="songsBar">
+                  <div class="left" @click="playMusicAll()">
+                    <i class="iconfont iconbofang-"></i>
+                    <span>播放全部 (共{{songInformation.trackCount}}首)</span>
+                  </div>
+                  <div class="right">
+                    <i class="iconfont icongengduo1"></i>
+                    <span>多选</span>
+                  </div>
+                </div>
+
+                <!-- song 详情 -->
+                <div class="songWarp">
+                  <ul>
+                    <li v-for="item in songplayList" :key="item.id" @click="playMusic(item)">
+                      <div class="left">
+                        <img :src="item.al.picUrl" :alt="item.al.name" />
                       </div>
-                    </div>
-                  </li>
-                </ul>
+                      <div class="center">
+                        <p>
+                          <span>{{item.name}}</span>&nbsp;
+                          <span class="popularity">{{item.alia[0]}}</span>
+                        </p>
+                        <p>{{item.ar[0].name}}</p>
+                      </div>
+                      <div class="right">
+                        <div class="icon">
+                          <i class="iconfont iconbofang"></i>
+                          <i class="iconfont iconziyuan"></i>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
@@ -110,13 +112,14 @@ export default {
       backgroudURl: "",
       songplayList: [],
       trackIds: [],
-      songInformation: []
+      songInformation: [],
+      rendering: false
     };
   },
   filters: {
     //拦截器
     retainDoubleDigit: function(data) {
-      // 讲数据转万
+      // 将数据转万
       return Math.floor(data / 10000);
     }
   },
@@ -150,6 +153,7 @@ export default {
             let trackSongIdString = "";
             that.trackIds = res.data.playlist.trackIds;
             that.songInformation = res.data.playlist;
+            that.rendering = true;
 
             for (let i = 0; i < that.trackIds.length; i++) {
               trackSongId.push(that.trackIds[i].id);
